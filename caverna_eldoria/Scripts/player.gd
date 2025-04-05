39b6a48dd33521242412ecc4f5025b9e6ba22597
dudualbertti <dudsalbertti@gmail.com>
2025-04-05 19:56:49 -0300

    Movimento vertical e horizontal do player

--- caverna_eldoria/Scripts/player.gd
+++ caverna_eldoria/Scripts/player.gd
@@ -1,25 +1,78 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = 300.0
+var velocity_change_speed = 20
+
+var jump_velocity = -400
+var gravity = 10
+
+var vertical_velocity = 0
+
+var jumping = false
+var can_jump = true
+
+@onready var jump_buffer_timer: Timer = $Jump_Buffer_Timer
+@onready var coyote_time_timer: Timer = $Coyote_Time_Timer
+
+
+func jump():
+	can_jump = false
+	vertical_velocity = jump_velocity
+
+
+func handle_jump_buffer(jump_press):
+	
+	if jump_press and not is_on_floor() and not jumping:
+		jump_buffer_timer.start()
+		
+	if is_on_floor() and not jump_buffer_timer.is_stopped():
+		jump()
+		
+
+func handle_coyote_time():
+	if coyote_time_timer.is_stopped() and can_jump:
+		coyote_time_timer.start()
+	
+	print(jump_buffer_timer.is_stopped())
+	
+func handle_vertical_movement():
+	
+	var jump_press = Input.is_action_pressed("jump")
+	
+	if is_on_floor():
+		can_jump = true
+		vertical_velocity = 0
+	else:
+		vertical_velocity += gravity
+		handle_coyote_time()
+	
+	if jump_press and can_jump:
+		jump()
+	
+	handle_jump_buffer(jump_press)
+	
+	if Input.is_action_just_released("jump") and jumping:
+		vertical_velocity = gravity
+
+	
+	jumping = velocity.y < 0 and not is_on_floor()
+	
+	velocity.y = vertical_velocity + gravity
+
+func handle_horizontal_movement():
+	var direction := Input.get_axis("left", "right")
+
+	velocity.x = move_toward(velocity.x, direction*speed, velocity_change_speed)
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	handle_horizontal_movement()
+	handle_vertical_movement()
 
 	move_and_slide()
+
+
+func _on_coyote_time_timer_timeout() -> void:
+	can_jump = false
