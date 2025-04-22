6cad80335360702781551263d02ceff0c92ff4ea
dudualbertti <dudsalbertti@gmail.com>
2025-04-21 23:28:19 -0300

    level 3 completo

--- caverna_eldoria/Scripts/global.gd
+++ caverna_eldoria/Scripts/global.gd
@@ -3,11 +3,27 @@ extends Node
 var current_scene = "res://Scenes/Menus/menu.tscn"
 var last_scene = current_scene
 
-var current_time = 20
+var current_name = ""
+
+var score_board = {}
+
+var current_time = 0
 var pontos = 0
 
 var rand = RandomNumberGenerator.new()
 
+func _ready():
+	load_game()
+
+
+func load_game():
+	if not FileAccess.file_exists("user://savegame.save"):
+		return
+		
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	var json = JSON.new()
+	score_board = json.parse_string(save_file.get_as_text())
+	
 
 func goto_scene(scene_path):
 	TransitionScreen.transition()
@@ -15,3 +31,13 @@ func goto_scene(scene_path):
 	last_scene = current_scene
 	current_scene = scene_path
 	get_tree().change_scene_to_file(scene_path)
+
+
+func save_game():
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	if current_name not in score_board:
+		score_board[current_name] = pontos
+	else:
+		if pontos > score_board[current_name]:
+			score_board[current_name] = pontos
+	save_file.store_string(JSON.stringify(score_board))
